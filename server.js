1598a8104bcf02f82699cbd4f0e867d1fb67cb46
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-01 10:58:47 -0500

    app update

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 
 app.get('/',(request, response)=>{
